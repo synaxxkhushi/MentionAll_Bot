62c72cb52ade692cee6abb2be46db1f76be20c4a
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-03-31 00:25:45 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,26 +21,26 @@ spam_chats = []
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
   await event.reply(
-    "__**I'm MentionAll Bot**, I will help you to mention near about all members in your group and channel 👻\nClick **/help** for more information__\n\n Follow [@The_Shikarii](https://github.com/ShikariBaaZ) on Github",
+    "__**I'm MentionAll Bot**, I will help you to mention near about all members in your group and channel 👻\nClick **/help** for more information__\n\n Follow [Sanatani Synax](https://www.instagram.com/sanatanisynax?igsh=YzljYTk1ODg3Zg==) on Instagram",
     link_preview=False,
     buttons=(
       [
-        Button.url('📣 Channel', 'https://t.me/The_SHIKARI_Network'),
-        Button.url('📦 Source', 'https://github.com/ShikariBaaZ/MentionAll_Bot')
+        Button.url('🦋 Support 🦋', 'https://t.me/synaxchatgroup'),
+        Button.url('🦋 Update 🦋', 'https://t.me/synaxnetwork')
       ]
     )
   )
 
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "**Help Menu of MentionAll_Bot**\n\nCommand: /mentionall\n__You can use this command with text what you want to say to others.__\n`Example: /mentionall ShikariBaaZ is Always on Shikar!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nFollow [@The_Shikarii](https://github.com/ShikariBaaZ) on Github"
+  helptext = "**Help Menu of MentionAll_Bot**\n\nCommand: /mentionall\n__You can use this command with text what you want to say to others.__\n`Example: /mentionall Synax Always on Top!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nFollow [Sanatani Synax](https://www.instagram.com/sanatanisynax?igsh=YzljYTk1ODg3Zg==) on Instagram"
   await event.reply(
     helptext,
     link_preview=False,
     buttons=(
       [
-        Button.url('📣 Channel', 'https://t.me/The_SHIKARI_Network'),
-        Button.url('📦 Source', 'https://github.com/ShikariBaaZ/MentionAll_Bot')
+        Button.url('🌸 Support 🌸', 'https://t.me/synaxchatgroup'),
+        Button.url('🌸 Update 🌸', 'https://t.me/synaxnetwork')
       ]
     )
   )
